d572f07903865aefc1b0cc3a38c60e5c93acf535
Neeraj Tickoo <ntickoo@gmail.com>
2022-02-12 01:20:19 -0500

    Web Layer unit tests

--- src/main/java/com/company/assetmanagment/data/dto/AssetIncomingDto.java
+++ src/main/java/com/company/assetmanagment/data/dto/AssetIncomingDto.java
@@ -1,5 +1,8 @@
 package com.company.assetmanagment.data.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,9 +10,14 @@ import lombok.Setter;
 @Setter
 public class AssetIncomingDto
 {
+	@NotEmpty
+	@Size(min = 3, message = "Asset name should be atleast 3 characters.")
 	private String name;
-	
+
+	@NotEmpty
+	@Size(min = 3, message = "Asset description should be atleast 3 characters.")
 	private String description;
 	
+	@NotEmpty
 	private String type;
 }
--- src/test/java/com/company/assetmanagment/controller/AssetControllerTest.java
+++ src/test/java/com/company/assetmanagment/controller/AssetControllerTest.java
@@ -1,10 +1,15 @@
 package com.company.assetmanagment.controller;
 
+import static org.mockito.AdditionalMatchers.not;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Date;
@@ -25,9 +30,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.company.assetmanagment.data.domain.model.Asset;
+import com.company.assetmanagment.data.dto.AssetIncomingDto;
 import com.company.assetmanagment.data.dto.MapStructMapper;
 import com.company.assetmanagment.data.dto.MapStructMapperImpl;
+import com.company.assetmanagment.exception.ResourceNotFoundException;
 import com.company.assetmanagment.service.AssetService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(controllers = AssetController.class)
 @ActiveProfiles("test")
@@ -53,27 +61,30 @@ class AssetControllerTest
 	@Autowired
 	private MockMvc mockMvc;
 
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@BeforeEach
 	public void setUp()
 	{
-		Asset asst1 = Asset.builder().name("TEST1").description("TEST DESCRIPTION1").type("TESTTYPE")
-				.id(Long.valueOf(1)).createdOn(new Date()).updatedOn(new Date()).build();
+		Asset asst1 = Asset.builder().name("TEST1").description("TEST DESCRIPTION1").type("TESTTYPE").id(Long.valueOf(1)).createdOn(new Date()).updatedOn(new Date()).build();
 
-		Asset asst2 = Asset.builder().name("TEST2").description("TEST DESCRIPTION2").type("TESTTYPE")
-				.id(Long.valueOf(2)).createdOn(new Date()).updatedOn(new Date()).build();
+		Asset asst2 = Asset.builder().name("TEST2").description("TEST DESCRIPTION2").type("TESTTYPE").id(Long.valueOf(2)).createdOn(new Date()).updatedOn(new Date()).build();
 
-		Asset asst3 = Asset.builder().name("TEST3").description("TEST DESCRIPTION3").type("TESTTYPE")
-				.id(Long.valueOf(3)).createdOn(new Date()).updatedOn(new Date()).build();
+		Asset asst3 = Asset.builder().name("TEST3").description("TEST DESCRIPTION3").type("TESTTYPE").id(Long.valueOf(3)).createdOn(new Date()).updatedOn(new Date()).build();
 
 		List<Asset> manyAssets = Stream.of(asst3, asst2, asst1).collect(Collectors.toList());
 
-		Mockito.when(assetService.getById(any(Long.class))).thenReturn(asst1);
-
+		
+		Mockito.when(assetService.getById( not(eq(Long.valueOf(100))) )   ).thenReturn(asst1);
+		Mockito.when(assetService.getById( Long.valueOf(100))	   		  ).thenThrow(new ResourceNotFoundException("No asset found with the id."));
+		
+		
 		Mockito.when(assetService.create(any(Asset.class))).thenReturn(asst2);
 
-		Mockito.when(assetService.update(anyLong(), any(Asset.class))).thenReturn(asst3);
-
+		Mockito.when(assetService.update(not(eq(Long.valueOf(100))), any(Asset.class)) ).thenReturn(asst3);
+		Mockito.when(assetService.update(    eq(Long.valueOf(100)) , any(Asset.class)) ).thenThrow(new ResourceNotFoundException("No asset found with the id."));
+		
 		doNothing().when(assetService).delete(any());
 
 		Mockito.when(assetService.findAllAndSortBy(any(Sort.class))).thenReturn(manyAssets);
@@ -81,14 +92,107 @@ class AssetControllerTest
 	}
 
 	@Test
-	public void shouldReturnDefaultMessage() throws Exception
-	{
-		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isNotFound());
-	}
-
-	@Test
-	public void shouldReturnData() throws Exception
+	public void SmokeTest_GetCall_ShouldPass() throws Exception
 	{
 		this.mockMvc.perform(get(API_PATH)).andDo(print()).andExpect(status().isOk());
 	}
+	
+	
+	@Test
+	public void GetApiTest_ByIdExists_ShouldPass() throws Exception
+	{
+		this.mockMvc.perform(get(API_PATH+"/1")).andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(jsonPath("$.id").value(1));
+	}
+	
+	
+	@Test
+	public void GetApiTest_ByIdNotExist_ShouldPass() throws Exception
+	{
+		this.mockMvc.perform(get(API_PATH+"/100")).andDo(print())
+					.andExpect(status().isNotFound())
+					.andExpect(jsonPath("$.status").value("NOT_FOUND"));
+	}
+	
+	@Test
+	public void GetAllApiTest_AnySort_ShouldPass() throws Exception
+	{
+		this.mockMvc.perform(get(API_PATH+"?sortDirection=ASC&sortColumn=type")).andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(jsonPath("$[0].id").value(3));
+	}
+	
+	
+	@Test
+	public void PostApiTest_ValidRequest_ShouldPass() throws Exception
+	{
+		AssetIncomingDto dto = new AssetIncomingDto();
+		dto.setName("ttt");
+		dto.setDescription("tttttt");
+		dto.setType("t");
+		
+		this.mockMvc.perform(post(API_PATH).contentType("application/json").content(objectMapper.writeValueAsString(dto)))
+					.andExpect(status().isCreated())
+					.andExpect(jsonPath("$.id").exists());
+	}
+	
+	@Test
+	public void PostApiTest_BadRequest_ShouldPass() throws Exception
+	{
+		AssetIncomingDto dto = new AssetIncomingDto();
+		dto.setName("t");
+		dto.setDescription("t");
+		dto.setType("t");
+		
+		this.mockMvc.perform(post(API_PATH)
+				.contentType("application/json")
+				.content(objectMapper.writeValueAsString(dto))).andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.status").value("BAD_REQUEST"));
+	}
+	
+	@Test
+	public void PutApiTest_ValidRequest_ShouldPass() throws Exception
+	{
+		AssetIncomingDto dto = new AssetIncomingDto();
+		dto.setName("ttt");
+		dto.setDescription("tttttt");
+		dto.setType("t");
+		
+		this.mockMvc.perform(put(API_PATH+"/1").contentType("application/json").content(objectMapper.writeValueAsString(dto)))
+					.andExpect(status().isOk())
+					.andExpect(jsonPath("$.id").exists());
+	}
+	
+	@Test
+	public void PutApiTest_AssetNotExist_ShouldPass() throws Exception
+	{
+		AssetIncomingDto dto = new AssetIncomingDto();
+		dto.setName("ttt");
+		dto.setDescription("tttttt");
+		dto.setType("t");
+		
+		this.mockMvc.perform(put(API_PATH+"/100").contentType("application/json").content(objectMapper.writeValueAsString(dto)))
+					.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void DeleteApiTest_Any_ShouldPass() throws Exception
+	{		
+		this.mockMvc.perform(delete(API_PATH+"/100").content(""))
+					.andExpect(status().is(204));
+	}
+	
+	@Test
+	public void PatchApiTest_AssetExist_ShouldPass() throws Exception
+	{
+		AssetIncomingDto dto = new AssetIncomingDto();
+		dto.setName("ttt");
+		dto.setDescription("tttttt");
+		dto.setType("t");
+		
+		this.mockMvc.perform(put(API_PATH+"/1").contentType("application/json").content(objectMapper.writeValueAsString(dto)))
+					.andExpect(status().isOk());
+	}
+	
 }
