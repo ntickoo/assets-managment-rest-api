8ffe49e52e4413f55683623420955bb4c4a970c0
Neeraj Tickoo <ntickoo@gmail.com>
2022-02-11 23:56:23 -0500

    Add Unit tests

--- src/test/java/com/company/assetmanagment/controller/AssetControllerTest.java
+++ src/test/java/com/company/assetmanagment/controller/AssetControllerTest.java
@@ -1,26 +1,84 @@
 package com.company.assetmanagment.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.company.assetmanagment.data.domain.model.Asset;
+import com.company.assetmanagment.data.dto.MapStructMapper;
+import com.company.assetmanagment.data.dto.MapStructMapperImpl;
+import com.company.assetmanagment.service.AssetService;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = AssetController.class)
 @ActiveProfiles("test")
 class AssetControllerTest
 {
-	private final String API_PATH = "/api/v1/assets";
+
+	@TestConfiguration
+	public static class MapStructMapperConfiguration
+	{
+		@Bean
+		public MapStructMapper getMapStructMapper()
+		{
+			return new MapStructMapperImpl();
+		}
+	}
+
+	@MockBean
+	private AssetService assetService;
+
 	
+	private final String API_PATH = "/api/v1/assets";
+
 	@Autowired
 	private MockMvc mockMvc;
+
+
+	@BeforeEach
+	public void setUp()
+	{
+		Asset asst1 = Asset.builder().name("TEST1").description("TEST DESCRIPTION1").type("TESTTYPE")
+				.id(Long.valueOf(1)).createdOn(new Date()).updatedOn(new Date()).build();
+
+		Asset asst2 = Asset.builder().name("TEST2").description("TEST DESCRIPTION2").type("TESTTYPE")
+				.id(Long.valueOf(2)).createdOn(new Date()).updatedOn(new Date()).build();
+
+		Asset asst3 = Asset.builder().name("TEST3").description("TEST DESCRIPTION3").type("TESTTYPE")
+				.id(Long.valueOf(3)).createdOn(new Date()).updatedOn(new Date()).build();
+
+		List<Asset> manyAssets = Stream.of(asst3, asst2, asst1).collect(Collectors.toList());
+
+		Mockito.when(assetService.getById(any(Long.class))).thenReturn(asst1);
+
+		Mockito.when(assetService.create(any(Asset.class))).thenReturn(asst2);
+
+		Mockito.when(assetService.update(anyLong(), any(Asset.class))).thenReturn(asst3);
+
+		doNothing().when(assetService).delete(any());
+
+		Mockito.when(assetService.findAllAndSortBy(any(Sort.class))).thenReturn(manyAssets);
+
+	}
 
 	@Test
 	public void shouldReturnDefaultMessage() throws Exception
